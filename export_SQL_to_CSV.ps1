eb185ea073865f1ae9d4087bc095423b81a7e322
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2023-10-09 15:21:42 +0200

    Add files via upload

---
+++ export_SQL_to_CSV.ps1
@@ -0,0 +1,51 @@
+﻿##################
+##  Input Data  ##
+##################
+$SQLServer = "server"  
+$SQLDBName = "database"   
+$CSV_Name = "csv_filename"
+$CSV_Path = "C:\DV"
+$delimiter = ";"
+
+#################
+##  SQL Query  ##
+#################
+
+#$SqlQuery = Get-Content -Path C:\DV\new.sql -Raw
+
+$SqlQuery = "SELECT * FROM Person"
+
+########################################################
+##  After this point, please do not modify anything!  ##
+########################################################
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection  
+$SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName;  trusted_connection = true"  #Integrated Security = true;
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand  
+
+$SqlCmd.Connection = $SqlConnection  
+$SqlCmd.CommandText = $SqlQuery  
+$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter  
+$SqlAdapter.SelectCommand = $SqlCmd
+
+#Concatenate Filepath
+$Export_Name = $CSV_Name + ".csv"
+$PathToSave = $CSV_Path + '\'+ $Export_Name
+
+
+#Creating Dataset
+$DataSet = New-Object System.Data.DataSet  
+$SqlAdapter.Fill($DataSet)
+$DataSet.Tables.Count
+try{
+    if(!(Test-Path -Path $PathToSave)){
+        New-Item -ItemType Directory -Force -Path $CSV_Path
+    }
+    $DataSet.Tables[0] | export-csv -Path $PathToSave -NoTypeInformation -Delimiter $delimiter
+}catch{
+    throw "Error!"
+}
+
+#Closes Connection
+$SqlConnection.Close()
+
+#$DataSet.Tables[0] | export-csv -Path $PathToSave -NoTypeInformation -Delimiter $delimiter
